4fb62e19392afb9dd31cf1cbb04021a89402cb67
huckssr <huckssr@gmail.com>
2017-01-18 14:00:34 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
-  
-  resources :jobs
+
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
 end
